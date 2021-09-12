9004df35db3a5cadb034e5f9b58e55955873e18a
Vitor R. Bizzi <vitorbizzi5@gmail.com>
2021-09-12 05:32:41 -0300

    Changing the data file_1 and exercises

---
+++ inicio/desafio_area.js
@@ -0,0 +1,22 @@
+/*Pi * Raio * Raio
+
+const pi = 3.14;
+let raio = 10;
+
+// A = pi . R²
+
+let calculoRaio = raio * raio;
+console.log(calculoRaio);
+
+calculoPi = calculoRaio * pi;
+
+console.log(calculoPi);
+
+console.log("O valor da Área é 314m²");*/
+
+//Math.PI
+
+const PI = 3.14;
+let raio = 10;
+resulCir = PI * raio * raio;
+console.log("O valor da área é " + resulCir +"m²");
---
+++ inicio/desafio_troca.js
@@ -0,0 +1,20 @@
+let a = 7;
+let b = 94;
+
+// depois da troca.... a = 94 b = 7
+
+a = 94;
+b = 7;
+
+console.log(a);
+console.log(b);
+
+/*Outro método é 
+
+let tempo = a;
+
+a = b;
+b = tempo
+
+console.log(a);
+console.log(b);*/
